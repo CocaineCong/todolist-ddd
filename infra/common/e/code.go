70d0b39c04dae61f9ff20dca8dec14c7ea35e52a
FanOne <294350394@qq.com>
2025-08-04 01:19:26 +0800

    feat:style code comment

--- infra/common/e/code.go
+++ infra/common/e/code.go
@@ -5,14 +5,13 @@ const (
 	ERROR         = 500
 	InvalidParams = 400
 
-	//成员错误
-	ErrorExistUser      = 10002
+	ErrorExistUser      = 10002 // 成员错误
 	ErrorNotExistUser   = 10003
 	ErrorFailEncryption = 10006
 	ErrorNotCompare     = 10007
 
-	ErrorAuthCheckTokenFail    = 30001 //token 错误
-	ErrorAuthCheckTokenTimeout = 30002 //token 过期
+	ErrorAuthCheckTokenFail    = 30001 // token 错误
+	ErrorAuthCheckTokenTimeout = 30002 // token 过期
 	ErrorAuthToken             = 30003
 	ErrorAuth                  = 30004
 	ErrorDatabase              = 40001
