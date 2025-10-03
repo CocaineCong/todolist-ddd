181b0b520512f8c829146fda7dccca4e16c92d51
FanOne <294350394@qq.com>
2025-10-03 15:56:57 +0800

    feat:api

--- interfaces/adapter/initialize/routes.go
+++ interfaces/adapter/initialize/routes.go
@@ -10,7 +10,7 @@ import (
 func NewRouter() *gin.Engine {
 	r := gin.Default()
 	r.Use(middleware.Cors())
-	v1 := r.Group("docs/v1/")
+	v1 := r.Group("api/v1/")
 	{
 		v1.GET("ping", func(c *gin.Context) {
 			c.JSON(200, "success")
